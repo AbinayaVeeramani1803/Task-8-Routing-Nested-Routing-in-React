78449f6cce493c0fe419759fd916694ebc502c94
Props Implementation in React Task 2 <abinayaveeramani@thestackly.com>
2026-06-18 22:47:35 +0530

    home.jsx

---
+++ home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <p>
+        Welcome to our React Router project. This page demonstrates
+        routing and nested routing concepts.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
