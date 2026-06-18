eaba288d7a8f285e0c5326f08a171d026df6df6b
Props Implementation in React Task 2 <abinayaveeramani@thestackly.com>
2026-06-18 22:53:15 +0530

    webdevelopment.jsx

---
+++ webdevelopment.jsx
@@ -0,0 +1,13 @@
+function WebDevelopment() {
+  return (
+    <div className="card">
+      <h3>Web Development</h3>
+      <p>
+        We create responsive websites using HTML, CSS, JavaScript,
+        React, and modern frameworks.
+      </p>
+    </div>
+  );
+}
+
+export default WebDevelopment;
