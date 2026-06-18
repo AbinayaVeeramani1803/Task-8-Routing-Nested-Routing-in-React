c179773d0cb45e48e6334c2b0367f3377c560429
Props Implementation in React Task 2 <abinayaveeramani@thestackly.com>
2026-06-18 22:45:52 +0530

    about.jsx

---
+++ about.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>
+        React Router DOM helps us create navigation between different
+        pages without reloading the browser.
+      </p>
+    </div>
+  );
+}
+
+export default About;
