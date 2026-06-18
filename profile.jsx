7a099729b3a5770dc70151354ebc6b9445a5f607
Props Implementation in React Task 2 <abinayaveeramani@thestackly.com>
2026-06-18 22:50:23 +0530

    profile.jsx

---
+++ profile.jsx
@@ -0,0 +1,12 @@
+function Profile() {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      <p>Name: Abinaya</p>
+      <p>Role: Front-End Developer</p>
+      <p>Location: Tamil Nadu</p>
+    </div>
+  );
+}
+
+export default Profile;
